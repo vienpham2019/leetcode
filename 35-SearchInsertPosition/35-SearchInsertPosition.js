776c7b64735612ecc6387239e9e4a6a0c0c7f743
vienpham2019 <vienpham2019@gmail.com>
2025-02-24 07:26:15 -0600

    File updated at 2/24/2025, 7:26:14 AM

--- 35-SearchInsertPosition/35-SearchInsertPosition.js
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -1,15 +1,23 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
+ * @param {number[]} digits
+ * @return {number[]}
  */
-var searchInsert = function(nums, target) {
-    let k = 0; 
-    for(let num of nums){
-        if(num >= target){
-            return k
+var plusOne = function(digits) {
+    let remain = 0;
+    for(let i = digits.length - 1; i >= 0; i --){
+          // add one to the last element of digits 
+        digits[i]+= 1
+        // if it greater than 9 set it 0 and remain 1 
+        if(digits[i] > 9){
+            digits[i] = 0 
+            remain = 1
+        }else {
+            return digits 
         }
-        k++
     }
-    return k
+    // if remain not 0 than push that to fist element 
+    if(remain > 0){
+        digits.unshift(remain)
+        return digits
+    }
 };
