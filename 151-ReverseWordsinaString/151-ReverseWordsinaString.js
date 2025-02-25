537ddff61f53691388dfd22cbe2de8e8a689e981
vienpham2019 <vienpham2019@gmail.com>
2025-02-25 11:44:50 -0600

    Create file

---
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s = s.split(' ').filter(word => word !== "")
+    let l = 0; 
+    let r = s.length - 1; 
+    while(l < r){
+        [s[l], s[r]] = [s[r], s[l]]
+        l ++; 
+        r --;
+    }
+    return s.join(' ')
+};
