d4892e1470cb2e7baeddc93b2a9b86314f4a6fc2
vienpham2019 <vienpham2019@gmail.com>
2025-02-25 11:07:22 -0600

    File updated at 2/25/2025, 11:07:22 AM

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.js
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.js
@@ -4,27 +4,10 @@
  * @return {string}
  */
 var gcdOfStrings = function (str1, str2) {
-    // let len1 = str1.length;
-    // let len2 = str2.length;
-    // if(len1 === len2 && str1 !== str2) return ""
-    // let isDivision = (l) => {
-    //     if (len1 % l !== 0 && len2 % l !== 0) return false;
-    //     return (
-    //          str1.slice(0, l) === str2.slice(0, l) &&
-    //         str1 === str1.slice(0, l).repeat(len1 / l) &&
-    //         str2 === str2.slice(0, l).repeat(len2 / l)
-    //     );
-    // };
-    // for (let i = Math.min(len1, len2); i > 0; i--) {
-    //     if (isDivision(i)) {
-    //         return str1.slice(0, i);
-    //     }
-    // }
-    // return "";
-
-     // Function to find the GCD of two numbers
-    function gcd(a, b) {
-        return b === 0 ? a : gcd(b, a % b);
+    // Function to find the GCD of two numbers
+    // GCD is Greatest Common Divisor Logic
+    function gcd(len1, len2) {
+        return len2 === 0 ? len1 : gcd(len2, len1 % len2);
     }
 
     // If str1 + str2 is not equal to str2 + str1, there's no common divisor
