1e36589ada90b76d17402c2bbd0b45714725abcf
vienpham2019 <vienpham2019@gmail.com>
2025-02-25 13:47:56 -0600

    File updated at 2/25/2025, 1:47:56 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.js
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.js
@@ -2,22 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var lengthOfLIS = function (nums) {
-    let res = [nums[0]]
+    // Step 1: Initialize the result array with the first element of nums.
+    // This array will store the smallest possible increasing subsequence.
+    let res = [nums[0]];
+
+    // Step 2: Iterate through the nums array, starting from the second element.
     for (let i = 1; i < nums.length; i++) {
+        // If the current number is greater than the last element in res, append 
+        // it.
         if (nums[i] > res[res.length - 1]) {
-            res.push(nums[i])
+            res.push(nums[i]);
         } else {
+            // Otherwise, replace the first element in res that is greater than 
+            // nums[i].
             for (let j = res.length - 1; j >= 0; j--) {
-                if(j === 0 && res[j] > nums[i]){
-                    res[j] = nums[i]
-                    break
-                }else if(res[j] > nums[i] && res[j - 1] < nums[i]){
-                    res[j] = nums[i]
-                    break
+                // If we reached the first element and it's greater than nums[i], 
+                // replace it.
+                if (j === 0 && res[j] > nums[i]) {
+                    res[j] = nums[i];
+                    break;
+                }
+                // If res[j] is greater than nums[i] but res[j - 1] is smaller, 
+                // replace res[j].
+                else if (res[j] > nums[i] && res[j - 1] < nums[i]) {
+                    res[j] = nums[i];
+                    break;
                 }
             }
         }
     }
-    return res.length
+
+    // Step 3: The length of res represents the length of the longest increasing 
+    // subsequence.
+    return res.length;
 };
