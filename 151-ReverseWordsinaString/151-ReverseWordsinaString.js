b15f7d3a807e8c447c0c5f081128b20b1fec9319
vienpham2019 <vienpham2019@gmail.com>
2025-02-25 11:45:57 -0600

    File updated at 2/25/2025, 11:45:57 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.js
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.js
@@ -3,13 +3,14 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    s = s.split(' ').filter(word => word !== "")
-    let l = 0; 
-    let r = s.length - 1; 
-    while(l < r){
-        [s[l], s[r]] = [s[r], s[l]]
-        l ++; 
-        r --;
-    }
-    return s.join(' ')
+    // s = s.split(' ').filter(word => word !== "")
+    // let l = 0; 
+    // let r = s.length - 1; 
+    // while(l < r){
+    //     [s[l], s[r]] = [s[r], s[l]]
+    //     l ++; 
+    //     r --;
+    // }
+    // return s.join(' ')
+    return s.trim().split(/\s+/).reverse().join(' ')
 };
