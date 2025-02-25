fe064f4e41819a6e53a041f0ed38113f584e1381
vienpham2019 <vienpham2019@gmail.com>
2025-02-25 10:28:31 -0600

    File updated at 2/25/2025, 10:28:31 AM

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.js
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.js
@@ -4,21 +4,37 @@
  * @return {string}
  */
 var gcdOfStrings = function (str1, str2) {
-    let len1 = str1.length;
-    let len2 = str2.length;
-    if(len1 === len2 && str1 !== str2) return ""
-    let isDivision = (l) => {
-        if (len1 % l !== 0 && len2 % l !== 0) return false;
-        return (
-             str1.slice(0, l) === str2.slice(0, l) &&
-            str1 === str1.slice(0, l).repeat(len1 / l) &&
-            str2 === str2.slice(0, l).repeat(len2 / l)
-        );
-    };
-    for (let i = Math.min(len1, len2); i > 0; i--) {
-        if (isDivision(i)) {
-            return str1.slice(0, i);
-        }
+    // let len1 = str1.length;
+    // let len2 = str2.length;
+    // if(len1 === len2 && str1 !== str2) return ""
+    // let isDivision = (l) => {
+    //     if (len1 % l !== 0 && len2 % l !== 0) return false;
+    //     return (
+    //          str1.slice(0, l) === str2.slice(0, l) &&
+    //         str1 === str1.slice(0, l).repeat(len1 / l) &&
+    //         str2 === str2.slice(0, l).repeat(len2 / l)
+    //     );
+    // };
+    // for (let i = Math.min(len1, len2); i > 0; i--) {
+    //     if (isDivision(i)) {
+    //         return str1.slice(0, i);
+    //     }
+    // }
+    // return "";
+
+     // Function to find the GCD of two numbers
+    function gcd(a, b) {
+        return b === 0 ? a : gcd(b, a % b);
     }
-    return "";
+
+    // If str1 + str2 is not equal to str2 + str1, there's no common divisor
+    if (str1 + str2 !== str2 + str1) {
+        return "";
+    }
+
+    // Find the greatest common divisor of the lengths of the strings
+    const gcdLength = gcd(str1.length, str2.length);
+    
+    // The common divisor string is the substring of str1 with length gcdLength
+    return str1.slice(0, gcdLength);
 };
