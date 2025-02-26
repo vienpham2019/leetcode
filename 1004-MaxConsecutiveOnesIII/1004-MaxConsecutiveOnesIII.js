57fc679c0124d354278ad8b2caa228f172771d92
vienpham2019 <vienpham2019@gmail.com>
2025-02-26 08:22:12 -0600

    File updated at 2/26/2025, 8:22:12 AM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.js
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.js
@@ -4,16 +4,28 @@
  * @return {number}
  */
 var longestOnes = function (nums, k) {
-    let max_w = l = numZeros = 0
-    for (let r = 0; r < nums.length; r++) {
-        if (nums[r] === 0) numZeros++
+    // Stores the maximum window size of consecutive 1s (including 
+    //k flips)
+    let max_w = 0; 
+    let l = 0; // Left pointer of the sliding window
+    let numZeros = 0; // Counts the number of zeros in the current window
 
-        while (numZeros > k) {
-            if (nums[l] === 0) numZeros--
-            l++
+    // Iterate through the array with the right pointer 'r'
+    for (let r = 0; r < nums.length; r++) {
+        // If the current element is 0, increase the zero count
+        if (nums[r] === 0) numZeros++;
+
+       // Only move 'l' when there are more than 'k' zeros in the window
+        if (numZeros > k) {
+            if (nums[l] === 0) numZeros--;
+            l++;
         }
 
-        max_w = Math.max(max_w , r - l + 1)
+        // Update the maximum window size
+        max_w = Math.max(max_w, r - l + 1);
     }
-    return max_w
+
+    // Return the maximum number of consecutive 1s that can be obtained by 
+    // flipping at most k zeros
+    return max_w;
 };
