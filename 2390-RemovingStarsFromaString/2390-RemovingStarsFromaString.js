ed7055a5887600642fb1df9103d410ea57f107b9
vienpham2019 <vienpham2019@gmail.com>
2025-02-27 13:11:14 -0600

    File updated at 2/27/2025, 1:11:14 PM

--- 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.js
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.js
@@ -2,17 +2,18 @@
  * @param {string} s
  * @return {string}
  */
-var removeStars = function(s) {
-    // using stack to store
-    // loop through string if that character is * then pop stack 
-    // else add to stack 
+var removeStars = function (s) {
+    // Iterate through s character by character.
+    // If the character is a normal letter, push it onto the stack.
+    // If the character is *, remove the last character from the stack (pop()).
+    // Finally, return the joined stack as the result.
     let stack = []
-    for(let char of s){
-        if(char === '*'){
-            stack.pop()
-        }else{
-            stack.push(char)
+    for (let char of s) {
+        if (char === "*") {
+            stack.pop(); // Remove the last added character
+        } else {
+            stack.push(char); // Add character to the stack
         }
     }
-    return stack.join('')
+    return stack.join(""); // Convert the stack back to a string
 };
